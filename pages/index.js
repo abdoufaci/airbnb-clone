af2285099692d29489c2a0bc2e270d41793dadd1
abdou <abdoufaci982@gmail.com>
2021-09-22 18:32:16 +0100

    'first'

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,6 @@ import MediumCard from '../components/MediumCard';
 import SmallCard from '../components/SmallCard';
 
 export default function Home({ exploreData, CardsData }) {
-  console.log(CardsData)
   return (
     <div>
       <Head>
